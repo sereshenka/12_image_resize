78ffbc8c524f61afd26c88965816c273d1a3053a
sereshenka <sergey.kolesnikov30@yandex.ru>
2016-12-14 13:10:54 +0300

    обновил все

--- image_resize.py
+++ image_resize.py
@@ -1,93 +1,65 @@
-import os
 import argparse
 from PIL import Image
+import os
 
 
-def input_all_arguments():
+def get_resized_image_size(size, args):
+    if args.scale and not args.height and not args.width:
+        return tuple(map(lambda x: int(x*args.scale), size))
+    if args.width and not args.height and not args.scale:
+        return tuple(map(lambda x: int(x*args.width/size[0]), size))
+    if args.height and not args.width and not args.scale:
+        return tuple(map(lambda x: int(x*args.height/size[1]), size))
+    if args.height and args.width and not args.scale:
+        if args.width/args.height != size[0]/size[1]:
+            print('scale is not the same')
+        return (int(args.width), int(args.height))
+    raise Exception('incorrect parameters')
+
+
+def get_resized_image_name(name, size):
+    dot_index = name.rfind('.')
+    res_name = '{}__{}x{}{}'.format(
+        name[:dot_index],
+        str(size[0]),
+        str(size[1]),
+        name[dot_index:]
+    )
+    return res_name
+
+
+def resize_image(args):
+    im = Image.open(args.original_path)
+    width, height = im.size
+    new_size = get_resized_image_size((width, height), args)
+    new_name = get_resized_image_name(os.path.basename
+                                      (args.original_path),
+                                      new_size)
+    if args.output:
+        path_to_resized = os.path.join(args.output, new_name)
+    else:
+        path_to_resized = os.path.join(os.path.dirname(args.original_path),
+                                       new_name)
+
+    im = im.resize(new_size)
+    im.save(path_to_resized)
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--original_path', help='Укажите путь к картинке ', \
+    parser.add_argument('--original_path', help='Укажите путь к картинке ', 
                         required = True, nargs = '?')
-    parser.add_argument('--width', help='Укажите ширину результирующей'\
-                        'картинки', nargs = '?')
-    parser.add_argument('--height', help='Укажите высоту результирующей'\
-                        'картинки ', nargs = '?')
-    parser.add_argument('--scale', help='Укажите во сколько раз'\
-                        'увеличить изображение ', nargs = '?')
-    parser.add_argument('--output', help='Укажите, куда класть'\
-                        'результирующий фаил ', nargs = '?')
-    original_path = parser.parse_args().original_path
-    width = parser.parse_args().width
-    height = parser.parse_args().height
-    scale = parser.parse_args().scale
-    output_path = parser.parse_args().output
-    return original_path, width, height, scale, output_path  
-
-
-def open_image(original_path):
-    try:
-        original_image = Image.open(original_path)
-        return original_image
-    except IOError:
-        return None
-
-
-def resize_with_scale(original_image, input_scale):
-    return Image.putdata(original_image, scale = input_scale)
-    
-
-def resize_with_width_or_height(original_image, width, height,\
-                                original_width, original_height):
-    proportions_warning = 0
-    if width is not None and height is not None:
-        proportions_width = width/original_width
-        proportions_height = height/original_height
-        if proportions_width != proportions_height:
-            raise proportions_warning
-        return original_image.resize(width, height), proportions_warning
-    elif width is not None:
-        new_height = original_height*(width/original_width)
-        return original_image.resize(width, new_height), proportions_warning
-    elif height is not None:
-        new_width = original_width*(height/original_height)
-        return original_image.resize(new_width, height), proportions_warning
+    parser.add_argument('--width', help='Укажите ширину результирующей'
+                        'картинки', type = int, nargs = '?')
+    parser.add_argument('--height', help='Укажите высоту результирующей'
+                        'картинки ', type = int, nargs = '?')
+    parser.add_argument('--scale', help='Укажите во сколько раз'
+                        'увеличить изображение ', type = float, nargs = '?')
+    parser.add_argument('--output', help='Укажите, куда сохранить ', nargs = '?')
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
-    while True:
-        original_path, width, height, scale, output_path = input_all_arguments()
-        if scale and (width or height):
-            print('нельзя вводить масштаб и ... одновременно')
-            break
-        if not width and not height and not scale:
-            print('Ничего не заданно')
-            break
-        if os.path.exists(original_path):
-            original_image = open_image(original_path)
-            original_width, original_height = original_image.size
-            if scale is not None:
-                try:
-                    round(scale)
-                except TypeError:
-                    print('Неверный формат')
-                    break
-                resize_with_scale(original_image, scale)
-            else:
-                if width:
-                    print('wow')
-                    try:
-                        width = int(width)
-                    except TypeError:
-                        print('Неверный формат')
-                        break
-                if height:
-                    try:
-                        height = int(height)
-                    except TypeError:
-                        print('Неверный формат')
-                        break
-                resize_with_width_or_height(original_image, width, height, \
-                                            original_width, original_height)
-        else:
-            print('не правильно задан путь')
-            break
-        break
+    args = parse_args()
+    resize_image(args)
